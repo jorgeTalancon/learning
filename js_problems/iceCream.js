c9033b59712d37dad77e33815930180b065f83e4
jorge <jorge.talancon@bloque9.com>
2023-04-19 22:50:01 -0600

    ice cream refactor

--- js_problems/iceCream.js
+++ js_problems/iceCream.js
@@ -44,10 +44,15 @@ var vessel = "cone";
 var toppings = "sprinkles";
 
 // Add your code here
-if (flavor === 'vanilla' || flavor === 'chocolate') {
+/* if (flavor === 'vanilla' || flavor === 'chocolate') {
     if (vessel === 'cone' || vessel === 'bowl') {
         if (toppings === 'sprinkles' || toppings === 'peanuts') {
             console.log('I\'d like two scoops of ' + flavor + ' ice cream in a ' + vessel + ' with ' + toppings + '.');
         }
     } 
+}
+*/
+
+if ((flavor === 'vanilla' || flavor === 'chocolate') && (vessel === 'cone' || vessel === 'bowl') && (toppings === 'sprinkles' || toppings === 'peanuts')) {
+    console.log('I\'d like two scoops of ' + flavor + ' ice cream in a ' + vessel + ' with ' + toppings + '.');
 }
